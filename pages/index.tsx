1c9cdf3a7be9624ffe12b203636474de63ae7200
Douglas Hellowell <doog99@gmail.com>
2021-09-26 12:59:20 +0100

    obey linting rules

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,17 +8,19 @@ const Home: NextPage = () => {
         <h2>
           Hey there! <span>👋</span>
         </h2>
-        <p>My name is Doug. (or Douglas, i really don't mind either way)</p>
         <p>
-          I'm a software developer & tutor at the Northcoders Bootcamp in
+          My name is Doug. (or Douglas, i really don&apos;t mind either way)
+        </p>
+        <p>
+          I&apos;m a software developer & tutor at the Northcoders Bootcamp in
           Manchester
         </p>
         <p>
-          This site is a space for me to share my ideas, what i'm working on and
-          to generally have a play with some cool new tech!
+          This site is a space for me to share my ideas, what i&apos;m working
+          on and to generally have a play with some cool new tech!
         </p>
 
-        <p>I've had fun building this site with:</p>
+        <p>I&apos;ve had fun building this site with:</p>
         <ul>
           <li>NextJS</li>
           <li>Typescript</li>
@@ -28,8 +30,8 @@ const Home: NextPage = () => {
         </ul>
 
         <p>
-          I'm always up for a chat - code and non-code related! Drop me a line
-          at: ____email here____
+          I&apos;m always up for a chat - code and non-code related! Drop me a
+          line at: ____email here____
         </p>
       </main>
     </div>
